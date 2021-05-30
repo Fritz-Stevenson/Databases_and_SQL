009a3cf4f6d9545bf4ff0f1d34f173e684abb13e
Fritz Stevenson <76401499+Fritz-Stevenson@users.noreply.github.com>
2021-05-29 23:10:49 -0700

    Update and rename wildfires.py to visualize_yearly_cause.py

--- Data-Project-Wildfires/visualize_yearly_cause.py
+++ Data-Project-Wildfires/visualize_yearly_cause.py
@@ -1,16 +1,8 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+
 plt.style.use('fivethirtyeight')
-
-
-def create_concat_csv():
-    "concatenates yearly csv files into a single file for insertion into a database"
-    csv_folder = '.\\yearly_OR_fires\\'
-    full_set_csv = pd.concat([pd.read_csv(csv_folder+file, sep=';') for file in os.listdir(csv_folder)])
-    print(full_set_csv.head(5))
-    full_set_csv.to_csv('OR_Fires.csv', index=False)
-    return full_set_csv
 
 def visualize_yearly_fire_cause():
     """
@@ -32,6 +24,6 @@ def visualize_yearly_fire_cause():
     plt.xlim([1993,2015])
     ax.legend(labels=['Arson', 'Children', 'Equipment Use', 'Lightning', 'Miscellaneous'])
     return plt.show()
-visualize_yearly_fire_cause()
 
-#print(plot_df.head(20))
+
+visualize_yearly_fire_cause()
