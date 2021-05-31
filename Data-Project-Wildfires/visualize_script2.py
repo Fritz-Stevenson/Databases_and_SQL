0f81ea7c948a77e7f03154e0d4b15d6aa99b88a8
Fritz Stevenson <76401499+Fritz-Stevenson@users.noreply.github.com>
2021-05-30 17:00:22 -0700

    Update and rename wildfires.py to visualize_script2.py

--- Data-Project-Wildfires/visualize_script2.py
+++ Data-Project-Wildfires/visualize_script2.py
@@ -8,36 +8,6 @@ import numpy as np
 from pyproj import Transformer
 plt.style.use('seaborn-pastel')
 
-
-def create_concat_csv():
-    "concatenates yearly csv files into a single file for insertion into a database"
-    csv_folder = '.\\yearly_OR_fires\\'
-    full_set_csv = pd.concat([pd.read_csv(csv_folder+file, sep=';') for file in os.listdir(csv_folder)])
-    print(full_set_csv.head(5))
-    full_set_csv.to_csv('OR_Fires.csv', index=False)
-    return full_set_csv
-
-def visualize_yearly_fire_cause():
-    """
-    Visualizing the yearly cause csv to highlight the drivers of historical burn trends
-    :return: The plot show function.
-    Also saves a png file of the plot.
-    """
-    fig, ax = plt.subplots(figsize=(20,20))
-    data = pd.read_csv('.\\CSV_Files\\yearly_fire_cause.csv')
-    data = data.loc[data['STAT_CAUSE_DESCR'].isin(['Lightning', 'Equipment Use', 'Miscellaneous', 'Children', 'Arson'])]
-    plot_df = pd.pivot_table(data,index=data['FIRE_YEAR'], columns= data['STAT_CAUSE_DESCR'])
-    ax.plot(range(1992,2016), plot_df)
-    ax.set_title('Yearly Burn Damage Organized by Cause')
-    ax.set_xlabel('Calendar Year')
-    ax.set_ylabel('Amount Burned (sq mi)')
-    ax.set_xticks(range(1992,2016))
-    ax.set_xticklabels(range(1992,2016))
-    plt.savefig('yearly_burn_damage_by_cause.png')
-    plt.xlim([1993,2015])
-    ax.legend(labels=['Arson', 'Children', 'Equipment Use', 'Lightning', 'Miscellaneous'])
-    return plt.show()
-
 def visualize_yearly_fire_scale():
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,10))
     data = pd.read_csv('.\\CSV_Files\\yearly_analysis.csv')
